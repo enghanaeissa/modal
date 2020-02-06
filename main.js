3587a37530609ff18abc13b9af26014fe73863d2
enghanaeissa <enghana487@gmail.com>
2020-02-07 00:42:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+/*select variables*/
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-botton]");
+const overlay = document.getElementById ('overlay');
+
+
+openModalButtons.forEach (button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+function openModal(modal){
+    if (modal == null)  return;
+        modal.classList.add("active");
+        overlay.classList.add("active");
+}
+
+
+overlay.addEventListener('click', () =>{
+    const modals = document.querySelectorAll(".modal.active");
+    modals.forEach(modal => {
+        closeModal(modal);
+        });
+    
+});
+closeModalButtons.forEach (button => {
+    button.addEventListener('click', () => {
+       const modal = button.closeset('.modal');
+        closeModal(modal);
+    });
+});
+
+function closeModal(modal){
+    if (modal == null)
+        return;
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+
+
+
